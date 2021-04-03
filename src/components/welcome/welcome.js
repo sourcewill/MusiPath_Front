a670b33d2bbccbd5f29618bde29fb0f86d6f27f4
williamrodrigues1997 <ra99514@uem.br>
2021-04-03 12:43:39 -0300

    Updating welcome

--- src/components/welcome/welcome.js
+++ src/components/welcome/welcome.js
@@ -7,9 +7,9 @@ class Welcome extends React.Component {
     render() {
         return (
             <div className='bemvindo'>
-                <h2>Bem-vindo(a)</h2>
-                <p>O Musipath oferece a interação e descoberta de novos artistas e músicas através da exploração de uma rede de artistas e músicas, criada automaticamente pelo sistema com base no seu estilo.</p>
-                <p><b>Comece agora mesmo sua interação com nossa rede de artistas e músicas! Inicie buscando pelo nome de um artista, como por exemplo "Coldplay".</b></p>
+                <h2>Bem-vindo(a)!</h2>
+                <p>O Musipath é um sistema em desenvolvimento que oferece a interação com uma rede de artistas e músicas, criada automaticamente! Explore álbuns e músicas que correspondem ao seu estilo.</p>
+                <p><b>Comece agora mesmo seu trajeto musical! Inicie buscando pelo nome de um artista como, por exemplo, "Coldplay" e conheça outros artistas similares.</b></p>
             </div>
         )
     }
