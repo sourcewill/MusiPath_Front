416063d1d5ed21db5131d1e6b4f3554a9fb98365
williamrodrigues1997 <ra99514@uem.br>
2021-04-05 09:14:07 -0300

    Upadating ngrok url

--- src/components/welcome/welcome.js
+++ src/components/welcome/welcome.js
@@ -9,7 +9,7 @@ class Welcome extends React.Component {
             <div className='bemvindo'>
                 <h2>Bem-vindo(a)!</h2>
                 <p>O Musipath é um sistema em desenvolvimento que oferece a interação com uma rede musical, criada automaticamente. Explore artistas, álbuns e músicas que correspondem ao seu estilo musical.</p>
-                <p>Inicie buscando pelo nome de um artista como, por exemplo, "Coldplay" e conheça outros artistas similares.</p>
+                <p>Inicie buscando pelo nome de um artista como, por exemplo, "Coldplay", "James Blunt", "Bruno Mars" e conheça outros artistas similares.</p>
                 <p><b>Comece agora mesmo seu trajeto pela música, desejamos uma ótima experiência!</b></p>
             </div>
         )
