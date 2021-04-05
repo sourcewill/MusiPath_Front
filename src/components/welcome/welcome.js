59db87cb7dfadbb3049b9d2b20b52a63113b0368
williamrodrigues1997 <ra99514@uem.br>
2021-04-05 09:52:25 -0300

    Updating welcome page

--- src/components/welcome/welcome.js
+++ src/components/welcome/welcome.js
@@ -9,8 +9,8 @@ class Welcome extends React.Component {
             <div className='bemvindo'>
                 <h2>Bem-vindo(a)!</h2>
                 <p>O Musipath é um sistema em desenvolvimento que oferece a interação com uma rede musical, criada automaticamente. Explore artistas, álbuns e músicas que correspondem ao seu estilo musical.</p>
-                <p>Inicie buscando pelo nome de um artista como, por exemplo, "Coldplay", "James Blunt", "Bruno Mars" e conheça outros artistas similares.</p>
-                <p><b>Comece agora mesmo seu trajeto pela música, desejamos uma ótima experiência!</b></p>
+                <p>Inicie buscando pelo nome de um artista como, por exemplo: "Jason Mraz", "Coldplay", "Bruno Mars" e conheça outros artistas similares a sua busca.</p>
+                <p><b>Comece agora mesmo seu trajeto pela música!</b></p>
             </div>
         )
     }
